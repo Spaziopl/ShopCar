a4af191b676a1a7dd52aa3389656d21e6ce2b84b
Spaziopl <Spazio308@gmail.com>
2025-10-15 00:50:27 +0200

    Trzeci commit: Zaaktualizowano Program, zmienne i pętla for

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string name = "Andrzej";
+int age = 23;
+double high = 1.85;
+bool isPrograming = true;
+
+Console.WriteLine($"Cześć! Mam na imię {name}, mam {age}, i mój wzrost to {high}m.");
+Console.WriteLine($"Czy lubię programować? Odpowiedż to: {isPrograming}");
+
+
+if (age >= 18)
+{
+    Console.WriteLine("Jesteś pełnoletni");
+}
+else Console.WriteLine("Jeszcze nie jesteś pelnoletni");
+
+for(int i = 0; i <= 5; i++)
+{
+    Console.WriteLine($"Licznik: {i}");
+}
