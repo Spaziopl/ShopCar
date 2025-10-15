8196c58073c3a2aaf7017d6a7d4e111904be7eb8
Spaziopl <Spazio308@gmail.com>
2025-10-15 03:22:24 +0200

    Nauka LINQ

---
+++ Program/Program/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebApp
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public int YearProduction { get; set; }
+        public Car(string brand, int yearProduction) 
+        {
+            Brand = brand;
+            YearProduction = yearProduction;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"This car is brand: {Brand}, and it create in {YearProduction}y.");
+        }
+    }
+}
--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -2,14 +2,53 @@
 
 
 using myApp.Models;
-
+using System.Linq;
+using MyWebApp;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Person person1 = new Person("Andrzej", 23);
-        person1.Introduce();
+        //List<Person> people = new List<Person>();
+
+
+        //people.Add(new Person("Andrzej", 23));
+        //people.Add(new Person("Marek", 12));
+        //people.Add(new Person("Mati", 3));
+
+        //var adults = people.Where(p => p.Age > 22).ToList();
+        //Console.WriteLine("Adults: ");
+        //foreach (var person in adults)
+        //{
+        //    person.Introduce();
+        //}
+
+        //var sortedPeople = people.OrderBy(p => p.Age).ToList();
+
+        //foreach(var person in sortedPeople)
+        //{
+        //    person.Introduce();
+        //}
+
+            List<Car> cars = new List<Car>
+    {
+        new Car("Audi", 2002),
+        new Car("BMW", 1999),
+        new Car("Toyota", 2015),
+        new Car("Toyota", 2009),
+        new Car("Mercedes", 1980),
+        new Car("Honda", 2020)
+    };
+        Console.WriteLine("");
+
+        var carList = cars.Where(p => p.Brand == "Toyota").OrderBy(x => x.YearProduction).ToList();
+
+        foreach (var car in carList) 
+        {
+            car.Introduce();
+        }
+
+
     }
 }
 
